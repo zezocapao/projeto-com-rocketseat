dad8e4d35cc989b140acd663de3d3d7e708f7358
José Tondin <josevictortondin2006@gmail.com>
2026-03-26 10:26:11 -0300

    Alteração ICONES

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/icon.jpg")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/icon.jpg")
   }
   /*Essa é a estrutura de como funciona, mas usando a função toggle fica mais simples. 
   if(html.classList.contains("light")) {
