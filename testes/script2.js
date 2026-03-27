a59a719ecb818beec38e416a63761154a874d6d6
José Tondin <josevictortondin2006@gmail.com>
2026-03-27 14:27:51 -0300

    Encerramento dos novos testes, indo para um novo projeto teste, se der certo, criarei uma nova repo.

--- testes/script2.js
+++ testes/script2.js
@@ -13,4 +13,11 @@ function toggleMod() {
 function hidding() {
   const esconde = document.getElementById("listas")
   esconde.classList.toggle("escondido")
+
+  const botao = document.getElementById("botao2")
+  if (esconde.classList.contains("escondido")) {
+   botao.textContent = "Mostrar Objetivos"
+  } else {
+   botao.textContent = "Esconde Objetivos"
+  }
 }
